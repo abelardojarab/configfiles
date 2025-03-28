01a2b3fb1495ac7332d5d9dbe62f9db5b9f9ab1c
Abelardo Jara-Berrocal <abelardo.jaraberrocal@arm.com>
2025-03-28 17:35:06 +0000

    Add static binaries

---
+++ binaries/amd64/btop/setuid.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:edf5333212e2c1844f39259c1426ad2795b7726684a0f1b2718673b24a66badb
+size 39
---
+++ binaries/amd64/btop/uninstall.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d411347832c51cb28711c1f49fa4120b24fa7a9db2a5efa67a5a4c563369e11
+size 42
